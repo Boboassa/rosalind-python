76bf65243e1694ea01dd4f0fde7b112ad9e1d308
Boboassa <uca.baldassi99@gmail.com>
2022-02-18 23:52:18 +0100

    Change code to avoid libraries
    Remove numpy library
    
    Update dataset.txt to latest attempt

--- 010-IEV/iev.py
+++ 010-IEV/iev.py
@@ -1,27 +1,29 @@
 # Read a 6 value vector representing the number of couples with different
 # genotypes and output the total number of offsprings with the dominant
 # phenotype
-import numpy as np
 
-# Read the vector
-#dataset = np.array([1, 0, 0, 1, 0, 1])
-dataset = np.loadtxt("dataset.txt")
-
-# Multiply by number of offspring per couple
+# Define number of offspring per couple
 OFFSPRING = 2
-dataset = dataset * OFFSPRING
 
-# Multiply by the probability of having a dominant phenotype offspring
-# AA x AA 1
-# AA x Aa 1
-# AA x aa 1
-# Aa x Aa .75
-# Aa x aa .5
-# aa x aa 0
+# Define probability of dominant offspring as:
+# AA x AA = 1
+# AA x Aa = 1
+# AA x aa = 1
+# Aa x Aa = 0.75
+# Aa x aa = 0.5
+# aa x aa = 0
 PROB = [1, 1, 1, 0.75, 0.5, 0]
 
-# Sum the different values
+# Read the vector
+with open("dataset.txt", "r") as f:
+    dataset = [int(x) for x in f.read().split()]
+
+# Multiply by number of offspring per couple
+dataset = [a * OFFSPRING for a in dataset]
+
+# Multiply by the probability of having a dominant phenotype offspring
+# and sum the different values
 total = sum([a*b for a, b in zip(dataset, PROB)])
 
-# Print
+# Print result
 print(total)
